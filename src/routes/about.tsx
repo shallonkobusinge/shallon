0d0132961b0a31e3875250455db5a2dc8afb8ed1
Shallon Kobusinge <50081035+shallonkobusinge@users.noreply.github.com>
2024-07-26 05:50:07 -0700

    feat: about

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -7,14 +7,14 @@ export default function About() {
     <div>
       <div className="xl:h-screen w-full lg:py-[4rem]">
         <Header title="About" optionalTitle="Me." color="white" />
-        <div className="xl:p-10 flex flex-col xl:w-24 xl:w-full xl:flex-row xl:gap-12 justify-center ">
+        <div className="xl:p-10 flex flex-col lg:w-24 xl:w-full xl:flex-row xl:gap-12 justify-center">
           <Image
             url="/images/portrait.png"
             alt="Shallon Kobusinge"
-            props="object-cover xl:w-72 xl:h-72 w-[15rem] mx-auto xl:mx-0 py-6 xl:py-0 h-[15em] rounded-md  xl:rounded-[100%]"
+            props="object-cover xl:w-72 xl:h-72 mx-auto xl:mx-0 rounded-md  xl:rounded-[100%]"
           />
 
-          <div className="mx-2 xl:w-2/4 w-full w-screen flex flex-col  xl:ml-10">
+          <div className="mx-2 xl:w-2/4 w-screen flex flex-col ">
             <div className="flex gap-2 ">
               <div className="w-[1rem] xl:h-[8rem] bg-primary p-1"></div>
               <div>
